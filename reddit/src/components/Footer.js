925ff29d5c2a2514d41f584a40c526e2fa39efc8
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-19 09:42:57 -0400

    update

--- reddit/src/components/Footer.js
+++ reddit/src/components/Footer.js
@@ -1,131 +1,129 @@
 import React from 'react'
 import {
-  AiOutlineTwitter,
   AiFillYoutube,
   AiFillInstagram,
-  AiFillHeart,
   AiFillFacebook,
   AiFillTwitterCircle,
   AiFillAmazonCircle,
 } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa";
+import Link from 'next/link';
 
 const Footer = () => {
   return (
  
         
-<footer class="bg-white dark:bg-gray-800 pt-4 pb-8 xl:pt-8">
-    <div class="max-w-screen-lg px-4 mx-auto text-gray-400 xl:max-w-screen-xl sm:px-6 md:px-8 dark:text-gray-300">
-        <ul class="flex flex-wrap justify-center pb-8 text-lg font-light">
-            <li class="w-1/2 md:w-1/3 lg:w-1/3">
-                <div class="text-center">
-                    <h2 class="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
+<footer className="bg-white dark:bg-gray-800 pt-4 pb-8 xl:pt-8">
+    <div className="max-w-screen-lg px-4 mx-auto text-gray-400 xl:max-w-screen-xl sm:px-6 md:px-8 dark:text-gray-300">
+        <ul className="flex flex-wrap justify-center pb-8 text-lg font-light">
+            <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                <div className="text-center">
+                    <h2 className="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
                         CATEGORY
                     </h2>
                     <ul>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Sports
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Money
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 News
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Tech
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li class="w-1/2 md:w-1/3 lg:w-1/3">
-                <div class="text-center">
-                    <h2 class="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
+            <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                <div className="text-center">
+                    <h2 className="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
                         CONTACTS
                     </h2>
                     <ul>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Youtube
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Instagram
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
-                                Tic Tok
-                            </a>
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
+                                TicTok
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 LinkedIn
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li class="w-1/2 md:w-1/3 lg:w-1/3">
-                <div class="text-center">
-                    <h2 class="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
+            <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                <div className="text-center">
+                    <h2 className="text-gray-500 dark:text-gray-200 text-md uppercase mb-4">
                         Join Us
                     </h2>
                     <ul>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Locations
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Career Opprotunities
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Sponsor
-                            </a>
+                            </Link>
                         </li>
-                        <li class="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
-                            <a href="#">
+                        <li className="mb-4 transition-colors duration-200 hover:text-gray-800 dark:hover:text-white">
+                            <Link href="#">
                                 Partner
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
             </li>
         </ul>
-        <div class="pt-8 flex border-t border-gray-200 max-w-xs mx-auto items-center justify-between">
-            <a href="#">
+        <div className="pt-8 flex border-t border-gray-200 max-w-xs mx-auto items-center justify-between">
+            <Link href="#">
             <AiFillAmazonCircle className="w-6 h-auto text-xl transition-colors duration-200 hover:text-gray-800 dark:hover:text-white" />
 
-            </a>
-            <a href="#">
+            </Link>
+            <Link href="#">
             <AiFillTwitterCircle className="w-6 h-auto text-xl transition-colors duration-200 hover:text-gray-800 dark:hover:text-white" />
 
-            </a>
-            <a href="#">
+            </Link>
+            <Link href="#">
             <AiFillFacebook className="w-6 h-auto text-xl transition-colors duration-200 hover:text-gray-800 dark:hover:text-white" />
-            </a>
-            <a href="#">
+            </Link>
+            <Link href="#">
             <AiFillInstagram className="w-6 h-auto text-xl transition-colors duration-200 hover:text-gray-800 dark:hover:text-white" />
 
-            </a>
-            <a href="#">
+            </Link>
+            <Link href="#">
             <AiFillYoutube className="w-6 h-auto text-xl transition-colors duration-200 hover:text-gray-800 dark:hover:text-white" />
-            </a>
+            </Link>
         </div>
-        <div class="text-center pt-10 sm:pt-12 font-light flex items-center justify-center">
+        <div className="text-center pt-10 sm:pt-12 font-light flex items-center justify-center">
             thejayadad
         </div>
     </div>
